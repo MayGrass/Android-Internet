cd2c0a509752dad5b37179b1de1fe8e2fa8cb2b6
DCH <dching0121@gmail.com>
2019-11-19 16:33:55 +0800

    Volley處理網頁文字，以及如何處理圖片並顯示

--- app/src/main/java/tw/org/iii/android_mysql_practice/MainActivity.java
+++ app/src/main/java/tw/org/iii/android_mysql_practice/MainActivity.java
@@ -1,26 +1,44 @@
 package tw.org.iii.android_mysql_practice;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
-import javax.net.ssl.HttpsURLConnection;
-
 public class MainActivity extends AppCompatActivity {
+    private RequestQueue queue;
+    private ImageView img;
+    private Bitmap bitmap;
+    private UIHandler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler = new UIHandler();
+        img = findViewById(R.id.img);
+        queue = Volley.newRequestQueue(this);
     }
 
     public void test1(View view) {
@@ -50,6 +68,59 @@ public class MainActivity extends AppCompatActivity {
             br.close();
         } catch (Exception e) {
             Log.v("DCH", e.toString());
+        }
+    }
+
+    public void test2(View view) {
+        Response.Listener<String> listener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.v("DCH", response);
+            }
+        };
+        Response.ErrorListener errorListener = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.v("DCH", "error" + error.toString());
+            }
+        };
+        StringRequest request = new StringRequest(Request.Method.GET,
+                "http://10.0.2.2/php_03",
+                listener,
+                errorListener);
+        queue.add(request);
+
+    }
+
+    public void test3(View view) {
+        new Thread() {
+            @Override
+            public void run() {
+                test13();
+            }
+        }.start();
+    }
+
+    //獲得圖片資訊
+    private void test13() {
+        try {
+            URL url = new URL("https://cdn.vox-cdn.com/thumbor/8Gm6NK0gcEvZsLT7Q2TJCTkcurQ=/0x0:2040x1360/1200x800/filters:focal(857x517:1183x843)/cdn.vox-cdn.com/uploads/chorus_image/image/65301238/wjoel_180413_1777_android_001.0.jpg");
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.connect();
+            //安卓的影像
+            bitmap = BitmapFactory.decodeStream(connection.getInputStream());
+            handler.sendEmptyMessage(0);
+
+        } catch (Exception e) {
+            Log.v("DCH", e.toString());
+        }
+    }
+
+    private class UIHandler extends Handler {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            img.setImageBitmap(bitmap);
         }
     }
 }
